7c977b96f302cc1cc7a5b2537f65c6e3e8e12890
risco@talktalk.net <risco@talktalk.net>
2019-09-01 15:17:31 +0100

    Add project files.

---
+++ BootstrapSite2/Controllers/OtherController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+
+namespace BootstrapSite2.Controllers
+{
+    
+    
+    public class OtherController : Controller
+    {
+        
+        public ActionResult FullWidth()
+        {
+            return View();
+        }
+    }
+}
---
+++ BootstrapSite2/Controllers/PortfolioController.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Mvc;
+
+
+namespace BootstrapSite2.Controllers
+{
+    
+    
+    public class PortfolioController : Controller
+    {
+        
+        public ActionResult OneColumn()
+        {
+            return View();
+        }
+        
+        public ActionResult TwoColumn()
+        {
+            return View();
+        }
+        
+        public ActionResult ThreeColumn()
+        {
+            return View();
+        }
+    }
+}
